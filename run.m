d6e3e85a95b1087067ed5e1d6df8e63a7dea3b48
ManCla <claudio.mandrioli@gmail.com>
2024-03-10 23:33:15 +0100

    fix main matlab script

--- run.m
+++ run.m
@@ -1,28 +1,9 @@
 
-% simulation duration
-test_duration = 10;
+% script to call the function for the simulation directly from matlab
+% useful for debugging
 
+% call function
+speed_output = run_sldemo_enginewc();
 
-% controller parameters
-Kp =0.0614; Ki = 0.0723;
-% Kp = [0.05 0.033 0.061];
-% Ki = [0.10 0.064 0.072];
-% Kp =0.0614; Ki = 0.0723;
-
-
-
-speed_setpoint = [[0,2000];
-                  [4.99,2000];
-                  [5,3000]
-                  [10,3000]];
-
-drag_torque_load = [[0,10];
-                    [3,10];
-                    [3.01,25];
-                    [7,25];
-                    [7.01,15];
-                    [10,15];];
-
-sim("sldemo_enginewc.slx")
-
+%plot output
 plot(speed_output)
